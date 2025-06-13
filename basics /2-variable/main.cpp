d810b6da2f895e6d42279bc5d0cf9d9c44493590
saken48 <162938047+saken48@users.noreply.github.com>
2025-06-13 18:06:51 +0700

    Create main.cpp

---
+++ basics /2-variable/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int fairy = 10;
+    double  tail =  3.14;
+    char letter = 'A';
+    bool  isThat = false;
+
+    cout << "Fairy: " << fairy << '\n';
+    cout << "Tail: " << tail << '\n';
+    cout << "Letter: " << letter << '\n';
+    cout << "Is that true? " << isThat << '\n';
+    // jika nilai boolean true, maka akan tampil 1
+    // jika nilai boolean false, maka akan tampil 0
+}
+
+
