b1ecc16db58b50497d1612e48eb2d788c086fcba
saken48 <162938047+saken48@users.noreply.github.com>
2025-06-13 18:08:03 +0700

    Create main.cpp

---
+++ basics /4-Operation/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a =  5,  b = 10;
+    cout << "The sum of a and b is: " << (a + b) << endl;
+    cout << "The product of a and b is: " << (a * b) << endl;
+    cout << "The difference of a and b is: " << (a - b) << endl;
+    cout << "The product of a and b is: " << (a * b) << endl;
+    cout << "The quotient of a and b is: " << (a / b) << endl;
+    cout << "The remainder of a and b is: " << (a % b) << endl;
+    cout << "The value of a after incrementing is: " << (++a) << endl;
+    cout << "The value of b after decrementing is: " << (--b) << endl;
+    cout << "The value of a after incrementing is: " << (++a) << endl;
+    cout << "The value of a after decrementing is: " << (--a) << endl;
+    cout  << "The value of b after incrementing is: " << (++b) << endl;
+    cout << "The value of b after decrementing is: " << (--b) << endl;
+
+    // logical operations
+
+    // comparison operations
+}
