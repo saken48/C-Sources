599a4d2ad035d8df813241f4e4626def1e2ca3bc
saken48 <162938047+saken48@users.noreply.github.com>
2025-06-13 18:08:54 +0700

    Create main.cpp

---
+++ basics /5-InOut/main.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+
+using namespace  std;
+
+int main() {
+    int angka ;
+    cout << "masukan angka : ";
+    cin >> angka;
+    cout << "angka yang anda masukan adalah: " << angka << endl;
+}
